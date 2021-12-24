14768b6aff0c842dfde65ef5a1e713b6925daadd
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-24 16:36:55 +0100

    [ADD] Main level first tile test
    > main level now adds grasswithseeds texture
    > main level uses factory to add tile game object

--- StrategyOfAttack/Scenes/MainLevel/MainLevel.cpp
+++ StrategyOfAttack/Scenes/MainLevel/MainLevel.cpp
@@ -1,6 +1,10 @@
 #include "MainLevel.h"
 
 #include <GameObject/GameObject.h>
+#include <TextureManager/TextureManager.h>
+#include <Texture/Texture.h>
+
+#include "../../Factories/Factories.h"
 
 MainLevel::MainLevel(const std::string& sceneName)
 	: Scene{ sceneName.c_str() }
@@ -8,5 +12,9 @@ MainLevel::MainLevel(const std::string& sceneName)
 
 void MainLevel::Start()
 {
+	using namespace Integrian2D;
 
+	TextureManager::GetInstance()->AddTexture("GrassWithSeeds", new Texture{ "Resources/GroundTextures/GrassWithSeeds.jpg" });
+
+	AddGameObject("Tile", Factories::CreateGrassTileWithSeeds(Point2f{}));
 }
