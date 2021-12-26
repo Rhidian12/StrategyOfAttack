95a245e3969aeae7f9fe7f7185172683a5f200c4
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-26 21:10:02 +0100

    [ADD/WIP] added MapComponent

--- StrategyOfAttack/GameComponents/MapComponent/MapComponent.cpp
+++ StrategyOfAttack/GameComponents/MapComponent/MapComponent.cpp
@@ -1,1 +1,23 @@
 #include "MapComponent.h"
+
+#include "../TileComponent/TileComponent.h"
+
+MapComponent::MapComponent(Integrian2D::GameObject* const pOwner, const std::vector<TileComponent*> tileComponents)
+	: Component{ pOwner }
+	, m_TileComponents{ tileComponents }
+{}
+
+Integrian2D::Component* MapComponent::Clone(Integrian2D::GameObject* const pOwner) noexcept
+{
+	return new MapComponent{ pOwner, m_TileComponents };
+}
+
+bool MapComponent::IsMovementLegal(const int tileIndex, const PlayerDirection direction) const noexcept
+{
+	return false;
+}
+
+int MapComponent::GetTileIndex(const Integrian2D::Point2f& location) const noexcept
+{
+	return 0;
+}
--- StrategyOfAttack/GameComponents/MapComponent/MapComponent.h
+++ StrategyOfAttack/GameComponents/MapComponent/MapComponent.h
@@ -1,5 +1,23 @@
 #pragma once
-class MapComponent
-{
-};
 
+#include "../../PlayerDirection/PlayerDirection.h"
+
+#include <Components/Component/Component.h>
+
+#include <vector>
+
+class TileComponent;
+
+class MapComponent final : public Integrian2D::Component
+{
+public:
+	MapComponent(Integrian2D::GameObject* const pOwner, const std::vector<TileComponent*> tileComponents);
+
+	virtual Component* Clone(Integrian2D::GameObject* const pOwner) noexcept override;
+
+	bool IsMovementLegal(const int tileIndex, const PlayerDirection direction) const noexcept;
+	int GetTileIndex(const Integrian2D::Point2f& location) const noexcept;
+
+private:
+	std::vector<TileComponent*> m_TileComponents;
+};
