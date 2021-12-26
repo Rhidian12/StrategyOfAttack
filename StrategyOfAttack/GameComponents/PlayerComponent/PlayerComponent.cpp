31d137f5805f5e16a1fa2ca63e39835eb56c9d58
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-26 21:10:13 +0100

    [ADD/WIP] Added PlayerComponent

--- StrategyOfAttack/GameComponents/PlayerComponent/PlayerComponent.cpp
+++ StrategyOfAttack/GameComponents/PlayerComponent/PlayerComponent.cpp
@@ -1,1 +1,37 @@
 #include "PlayerComponent.h"
+
+#include <GameObject/GameObject.h>
+#include <Components/TransformComponent/TransformComponent.h>
+#include <Input/InputManager/InputManager.h>
+
+PlayerComponent::PlayerComponent(Integrian2D::GameObject* const pOwner)
+	: Component{ pOwner }
+{}
+
+Integrian2D::Component* PlayerComponent::Clone(Integrian2D::GameObject* const pOwner) noexcept
+{
+	return new PlayerComponent{ pOwner };
+}
+
+void PlayerComponent::Start()
+{
+	using namespace Integrian2D;
+
+	InputManager* const pInputManager{ InputManager::GetInstance() };
+
+	pInputManager->AddAxis(InputAxis{ "VerticalMovement", GameInput{ KeyboardInput::W }, GameInput{ KeyboardInput::S } });
+	pInputManager->AddAxis(InputAxis{ "HorizontalMovement", GameInput{ KeyboardInput::D }, GameInput{ KeyboardInput::A } });
+}
+
+void PlayerComponent::Update()
+{
+	using namespace Integrian2D;
+
+	InputManager* const pInputManager{ InputManager::GetInstance() };
+
+	if (const int8_t value{ pInputManager->GetAxis("VerticalMovement") }; value != 0)
+		m_pOwner->pTransform->Translate(Vector2f{ 0.f, value * 50.f });
+
+	if (const int8_t value{ pInputManager->GetAxis("HorizontalMovement") }; value != 0)
+		m_pOwner->pTransform->Translate(Vector2f{ value * 50.f, 0.f });
+}
--- StrategyOfAttack/GameComponents/PlayerComponent/PlayerComponent.h
+++ StrategyOfAttack/GameComponents/PlayerComponent/PlayerComponent.h
@@ -4,4 +4,14 @@
 
 class PlayerComponent final : public Integrian2D::Component
 {
+public:
+	PlayerComponent(Integrian2D::GameObject* const pOwner);
+
+	virtual Component* Clone(Integrian2D::GameObject* const pOwner) noexcept override;
+
+	virtual void Start() override;
+
+	virtual void Update() override;
+
+private:
 };
